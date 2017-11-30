7b66a4bfdd876a2d44c01ca50f81c62b54d673a9
greyhats13 <imamariefrahmann@gmail.com>
2017-11-30 23:56:07 +0700

    fix current_date 4

--- operasional_kantor/pages/ajax/fetchdata/fetch_check_absensis_hari_ini.php
+++ operasional_kantor/pages/ajax/fetchdata/fetch_check_absensis_hari_ini.php
@@ -1,13 +1,14 @@
 <?php
 //fetch.php
 unset($_SESSION["isAbsenToday"]);
+$today1=date("Y-m-d");
 if(time() <= strtotime("09:00 AM")){
     $_SESSION["isAbsenToday"]=-1;
 } else {
     unset($_SESSION["isAbsenToday"]);
     $conn = createConn();
     $id_anggota = $_SESSION['id_anggota'];
-    $query = "SELECT id_anggota FROM tb_anggota WHERE id_anggota IN (SELECT id_anggota FROM tb_detail_absen WHERE id_anggota='$id_anggota' AND tanggal=CURRENT_DATE())";
+    $query = "SELECT id_anggota FROM tb_anggota WHERE id_anggota IN (SELECT id_anggota FROM tb_detail_absen WHERE id_anggota='$id_anggota' AND tanggal='$today1')";
     $res = $conn->query($query);
     $row = $res->num_rows;
     if($row>0){
