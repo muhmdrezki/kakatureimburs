019c4d99d018981ed56b5c50c3b005299b34b391
greyhats13 <imamariefrahmann@gmail.com>
2017-12-29 03:00:11 +0700

    Update tampilan Chart Credit hanya untuk status credit_stat paid

--- operasional_kantor/pages/ajax/fetchdata/fetch_chart-total-credit.php
+++ operasional_kantor/pages/ajax/fetchdata/fetch_chart-total-credit.php
@@ -2,11 +2,13 @@
  //fetch.php  
   include "../../../con_db.php";
   session_start();
+  date_default_timezone_set('Asia/Jakarta');
+  $today1 = date("Y-m-d");
   if (strpos($_SESSION['jabatan'], 'Admin')!==false) {
-    $sql_grafik6 =sprintf("SELECT SUBSTRING(MONTHNAME(`tanggal`),1,3) AS Bulan,SUM(credit_in) AS Total FROM tb_detail_absen WHERE YEAR(tanggal) = YEAR(CURRENT_DATE()) GROUP BY Bulan, MONTH(tanggal), YEAR(tanggal) ORDER BY Year(tanggal),MONTH(tanggal)");
+    $sql_grafik6 =sprintf("SELECT SUBSTRING(MONTHNAME(`tanggal`),1,3) AS Bulan,SUM(credit_in) AS Total FROM tb_detail_absen WHERE YEAR(tanggal) = YEAR('$today1') AND credit_stat='paid' GROUP BY Bulan, MONTH(tanggal), YEAR(tanggal) ORDER BY Year(tanggal),MONTH(tanggal)");
   //$sql_grafik6 = sprintf("SELECT SUBSTRING(MONTHNAME(`tanggal_set`),1,3) AS Bulan, SUM(`total_credit`) AS Total FROM `tb_credits_anggota` WHERE YEAR(`tanggal_set`) = '$year6' GROUP BY Bulan, MONTH(`tanggal_set`), YEAR(`tanggal_set`) ORDER BY Year(`tanggal_set`),MONTH(`tanggal_set`)");
   } else {
-    $sql_grafik6 =sprintf("SELECT SUBSTRING(MONTHNAME(`tanggal`),1,3) AS Bulan,SUM(credit_in) AS Total FROM tb_detail_absen WHERE YEAR(tanggal) = YEAR(CURRENT_DATE()) AND id_anggota='$_SESSION[id_anggota]' GROUP BY Bulan, MONTH(tanggal), YEAR(tanggal) ORDER BY Year(tanggal),MONTH(tanggal)");
+    $sql_grafik6 =sprintf("SELECT SUBSTRING(MONTHNAME(`tanggal`),1,3) AS Bulan,SUM(credit_in) AS Total FROM tb_detail_absen WHERE YEAR(tanggal) = YEAR('$today1') AND id_anggota='$_SESSION[id_anggota]' AND credit_stat='paid' GROUP BY Bulan, MONTH(tanggal), YEAR(tanggal) ORDER BY Year(tanggal),MONTH(tanggal)");
   }
   $hasil6 = $koneksi->query($sql_grafik6);
   if (!$hasil6) {
