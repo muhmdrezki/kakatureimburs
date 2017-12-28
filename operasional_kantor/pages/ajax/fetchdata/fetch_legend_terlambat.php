8eb2bff072349ca437309d7bd18e7aeb1078f22d
greyhats13 <imamariefrahmann@gmail.com>
2017-12-29 00:17:17 +0700

    Informasi tidak kerja pada gallery absensi hari ini 2

--- operasional_kantor/pages/ajax/fetchdata/fetch_legend_terlambat.php
+++ operasional_kantor/pages/ajax/fetchdata/fetch_legend_terlambat.php
@@ -1,17 +1,17 @@
 <?php
 //fetch.php
-$query = "SELECT COUNT(jam_masuk) AS value,'Ontime' AS label FROM tb_detail_absen WHERE DATE(tanggal)='$today' AND TIME(jam_masuk)<='10:00:00' AND status_id IN (1,3,7) GROUP BY tanggal";
+$query = "SELECT COUNT(jam_masuk) AS value,'Ontime' AS label FROM tb_detail_absen WHERE DATE(tanggal)='$today' AND TIME(jam_masuk)<='10:00:00' AND status_id IN (1,2,7) GROUP BY tanggal";
 $result = mysqli_query($koneksi, $query);
 $all = array();
 while ($row = mysqli_fetch_assoc($result)) {
     $all[] = $row;
 }
-$query2 = "SELECT COUNT(jam_masuk) AS value,'Late' AS label FROM tb_detail_absen WHERE DATE(tanggal)='$today' AND TIME(jam_masuk)>'10:00:00' AND status_id IN (1,3,7) GROUP BY tanggal";
+$query2 = "SELECT COUNT(jam_masuk) AS value,'Late' AS label FROM tb_detail_absen WHERE DATE(tanggal)='$today' AND TIME(jam_masuk)>'10:00:00' AND status_id IN (1,2,7) GROUP BY tanggal";
 $result2 = mysqli_query($koneksi, $query2);
 while ($row2 = mysqli_fetch_assoc($result2)) {
     $all[] = $row2;
 }
-$query3 = "SELECT COUNT(jam_masuk) AS value,'Tidak Kerja' AS label FROM tb_detail_absen WHERE DATE(tanggal)='$today' AND status_id NOT IN (1,3,7) GROUP BY tanggal";
+$query3 = "SELECT COUNT(jam_masuk) AS value,'Tidak Kerja' AS label FROM tb_detail_absen WHERE DATE(tanggal)='$today' AND status_id NOT IN (1,2,7) GROUP BY tanggal";
 $result3 = mysqli_query($koneksi, $query3);
 while ($row3 = mysqli_fetch_assoc($result3)) {
     $all[] = $row3;
