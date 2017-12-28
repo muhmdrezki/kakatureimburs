41674a11cb73890fb38f6aafe609e023fb9b13d6
greyhats13 <imamariefrahmann@gmail.com>
2017-12-29 02:05:59 +0700

    Informasi belum absen pada gallery absensi hari ini 11

--- operasional_kantor/pages/home.php
+++ operasional_kantor/pages/home.php
@@ -257,13 +257,13 @@
 								  </div>
 							  </div>
 							  <div class="col-md-8">
-									<div id="galleryFotoAbsensi"></div>
                   <ul class="chart-legend clearfix list-inline">
-                      <li id="listOntime"><span class="list-inline-item numberCircleAqua" id="jumOntime"></span>On Time</li>
-                      <li id="listLate"><span class="list-inline-item numberCircleOrange" id="jumLate"></span>Terlambat</li>
-                      <li id="listTidakKerja"><span class="list-inline-item numberCircleRed" id="jumTidakKerja"></span>Tidak Kerja</li>
-                      <li id="listBelumAbsen"><span class="list-inline-item numberCircleGray" id="jumBelumAbsen"></span>Belum Absen</li>
-                </ul>
+                        <li id="listOntime"><span class="list-inline-item numberCircleAqua" id="jumOntime"></span>On Time</li>
+                        <li id="listLate"><span class="list-inline-item numberCircleOrange" id="jumLate"></span>Terlambat</li>
+                        <li id="listTidakKerja"><span class="list-inline-item numberCircleRed" id="jumTidakKerja"></span>Tidak Kerja</li>
+                        <li id="listBelumAbsen"><span class="list-inline-item numberCircleGray" id="jumBelumAbsen"></span>Belum Absen</li>
+                  </ul>
+									<div id="galleryFotoAbsensi"></div>
                 </div>
                 <br>
 							  <!-- ./chart-responsive -->
